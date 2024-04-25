08e721e879c1ba8f8e3925402dc537a5e162ceb8
sun88110 <138852390+sun88110@users.noreply.github.com>
2024-04-25 13:55:31 +0900

    메쉬프로 타입변경

--- Escape_Room/Assets/Scripts/GameManager.cs
+++ Escape_Room/Assets/Scripts/GameManager.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public Text talkText;
+    public TextMeshProUGUI talkText;
     public GameObject scanObject;
 
     public void Action(GameObject scanObj)
