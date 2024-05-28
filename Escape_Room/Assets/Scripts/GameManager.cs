62765c1212543a14fa7b2f40c090fbfa2f80e960
Ori-hub-code <uic3883@gmail.com>
2024-05-28 15:13:47 +0900

    Map 브런치 Merge

---
+++ Escape_Room/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI talkText;
+    public GameObject scanObject;
+
+    public void Action(GameObject scanObj)
+    {
+        scanObject = scanObj;
+        talkText.text = "이것은 " + scanObj.name + "인 듯 하다.";
+    }
+}
