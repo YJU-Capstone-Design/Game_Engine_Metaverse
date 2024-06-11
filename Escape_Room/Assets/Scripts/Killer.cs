2120b86333c793e4570d523844ce537c813426e6
B-min512 <als0512123@gmail.com>
2024-06-11 14:13:13 +0900

    Temp

--- Escape_Room/Assets/Scripts/Killer.cs
+++ Escape_Room/Assets/Scripts/Killer.cs
@@ -87,40 +87,43 @@ public class Killer : MonoBehaviour
 
         foreach (RaycastHit hit in hits)
         {
-            GameObject hitPlayer = hit.transform.gameObject;
-            // 해당 Player의 방향벡터값 계산
-            Vector3 hitDir = (hitPlayer.transform.position - rayStart).normalized;
-            float hitRad = Mathf.Acos(Vector3.Dot(rayDir, hitDir));
-            float hitDeg = Mathf.Rad2Deg * hitRad;
-
-            // 해당 Player가 Killer의 시야각 내부에 위치할 경우
-            if (hitDeg >= leftDeg && hitDeg <= rightDeg)
+            if (!hit.transform.gameObject.activeSelf)
             {
-                // 해당 Player를 Target으로 설정할 지에 대한 코드
-                if (target != null)
-                {
-                    // 설정된 Target이 있을 경우 해당 Player와 Target의 거리 비교
-                    // Player와 Killer 간 거리가 더 가까운 Player를 Target으로 재설정 
-                    float targetDist = Vector3.Distance(rayStart, target.transform.position);
-                    float hitDist = Vector3.Distance(rayStart, hitPlayer.transform.position);
+                GameObject hitPlayer = hit.transform.gameObject;
+                // 해당 Player의 방향벡터값 계산
+                Vector3 hitDir = (hitPlayer.transform.position - rayStart).normalized;
+                float hitRad = Mathf.Acos(Vector3.Dot(rayDir, hitDir));
+                float hitDeg = Mathf.Rad2Deg * hitRad;
 
-                    if (hitDist < targetDist)
+                // 해당 Player가 Killer의 시야각 내부에 위치할 경우
+                if (hitDeg >= leftDeg && hitDeg <= rightDeg)
+                {
+                    // 해당 Player를 Target으로 설정할 지에 대한 코드
+                    if (target != null)
                     {
+                        // 설정된 Target이 있을 경우 해당 Player와 Target의 거리 비교
+                        // Player와 Killer 간 거리가 더 가까운 Player를 Target으로 재설정 
+                        float targetDist = Vector3.Distance(rayStart, target.transform.position);
+                        float hitDist = Vector3.Distance(rayStart, hitPlayer.transform.position);
+
+                        if (hitDist < targetDist)
+                        {
+                            target = hitPlayer;
+                        }
+                    }
+                    else
+                    {
+                        // 설정된 Target이 없을 경우 해당 Player를 Target으로 지정
                         target = hitPlayer;
                     }
                 }
                 else
                 {
-                    // 설정된 Target이 없을 경우 해당 Player를 Target으로 지정
-                    target = hitPlayer;
-                }
-            }
-            else
-            {
-                if (target != null)
-                {
-                    Vector3 targetDir = (target.transform.position - rayStart).normalized;
-                    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(targetDir), Time.deltaTime * 2.5f);
+                    if (target != null)
+                    {
+                        Vector3 targetDir = (target.transform.position - rayStart).normalized;
+                        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(targetDir), Time.deltaTime * 2.5f);
+                    }
                 }
             }
         }
